26e87486e68dbca21dbdcea8454de7f57112c336
Marlene Acosta <marleneacostah19@gmail.com>
2021-03-29 11:52:46 -0600

    Content-type utilizando Postman

--- nodejs/index.js
+++ nodejs/index.js
@@ -1,85 +1,88 @@
-
 const http = require('http');
 const url = require('url');
 const StringDecoder = require('string_decoder').StringDecoder;
 
-const callbackDelServidor = (req, res) => {
+  const callbackDelServidor = (req, res) => {
+   //obtener url desde el objeto request 
+    const urlActual = req.url;
+    const urlParseada = url.parse(urlActual, true);
 
-    //1.obtener url desde el objeto request
-    const urlActual= req.url;
-    const urlParseada =url.parse(urlActual, true);
+  //obtener la ruta
+  const ruta = urlParseada.pathname;
 
-    //2.obtener la ruta
-    const ruta =urlParseada.pathname;
-    
-    //3.quitar slash
-    const rutaLimpia = ruta.replace(/^\/+|\/+$/g, '');
+  //Enviar una respuesta dependiendo de la ruta que se le asigne
+  
 
-    //3.1Obtener el método http
-    const metodo = req.method.toLowerCase();
-    
-    //3.2 Obtener variables de query url
-    const  {query = {} } = urlParseada;
+  //Quitar el slash
+  const rutaLimpia = ruta.replace(/^\/+|\/+$/g, '');
 
-    //3.3 Obtener headers
-    const  {headers = {}} = req;
-    
-    //3.4Obtener payload, en caso de haber uno
-    const decoder = new StringDecoder('utf-8');
-    let buffer = '';
-    
-    //3.4.1 Ir acomodando la data cuando el request reciba un payload
-    req.on('data', (data) => {
-        buffer += decoder.write(data);
-    });
+  //Obtener el método http
+  const metodo =  req.method.toLowerCase();
 
-    //3.4.2 Terminar de acomodar datos y decirle al decoder que finalice
-    req.on('end', () => {
-        buffer += decoder.end();
+  //Obtener variables del query url
+  const { query = {} } = urlParseada;
 
-    //3.5 Ordenar la data del request
-    const data={
-        ruta: rutaLimpia,
-        query,
-        metodo,
-        headers,
-        payload: buffer
+  //Obtener los headers
+  const { headers = {} } = req;
+
+  //Obtener el payload
+  const decoder = new StringDecoder('utf-8');
+  let buffer = '';
+
+  //Ir acumulando la data cuando el request reciba un payload
+  req.on('data', (data)=>{
+    buffer += decoder.write(data);
+  });
+
+  //Se termina de acumular los datos y decirle al decoder que finalice
+  req.on('end', ()=>{
+    buffer += decoder.end();
+
+    //Ordenar la data 
+    const data = {
+      ruta: rutaLimpia,
+      query,
+      metodo,
+      headers,
+      payload: buffer
     };
 
-    //3.6 Elegir el manejador dependiendo de la ruta y asignarle la funcion que el enrutador tiene
+    //Elegir el manejador dependiendo de la ruta  //(handler) y asignarle función que el enrutador tiene 
     let handler;
-    if (rutaLimpia && enrutador[rutaLimpia]) {
-        handler = enrutador[rutaLimpia];
-    }else{
-        handler=enrutador.noEncontrado;
+    if(rutaLimpia && enrutador[rutaLimpia]) {
+      handler = enrutador[rutaLimpia];
+    } else {
+      handler = enrutador.noEncontrado;
     }
 
-    //4.Ejecutar handler (manejador) para ver la respuesta
-    if (typeof handler === 'function') {
-        handler(data, (statusCode = 200, mensaje)=>{
-            const respuesta = JSON.stringify(mensaje);
-            res.writeHead(statusCode);
-            //linea donde realmente estamos respondiendo a la aplicación cliente
-            res.end(respuesta);
-        })
+    //Ejecutar handler (manejador) para enviar respuesta
+    if(typeof handler === 'function') {
+      handler(data, (statusCode = 200, mensaje)=>{
+        const respuesta = JSON.stringify(mensaje);
+        res.setHeader("Content-Type", "application/json")
+        res.writeHead(statusCode);
+        // linea donde se responde a la aplicación cliente
+        res.end(respuesta);
+      })
     }
-    });
-    
+
+  });
 };
 
 const enrutador = {
-    ruta: (data, callback) => { 
-        callback(200, {mensaje: 'Esta es /ruta'});
-    },
-    usuarios: (data, callback) => { 
-        callback(200, [{nombre: 'usuario 1'},{nombre: 'usuario 2'}]);
-    },
-    noEncontrado: (data, callback) =>{
-        callback: (404, {mensaje:'No encontrado'});
-    }
+  ruta: (data, callback) => {
+    callback(200, {mensaje: 'Esta es /ruta'});
+  },
+  usuarios: (data, callback) => {
+    callback(200, [{nombre: 'usuario 1'},{nombre: 'usuario 2'}]);
+  },
+  noEncontrado: (data, callback) => {
+    callback(404, {mensaje: 'No encontrado'});
+  }
 }
 
 const server = http.createServer(callbackDelServidor);
+
 server.listen(5000, ()=>{
-    console.log('El servidor está escuchando peticiones en http://localhost:5000/');
+  console.log('El servidor esta escuchando peticiones en http://localhost:5000/');
 });
